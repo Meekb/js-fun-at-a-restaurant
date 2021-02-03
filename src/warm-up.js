5401efcfad8c85cfe5879081755c617ed701ed06
Beth Meeker <bethm.meeker@gmail.com>
2021-02-03 16:07:31 -0700

    Initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,26 +13,46 @@
 // var type = "Dessert";
 // var inStock = 10
 
+var food = "Pizza";
+var type = "Entree";
+var inStock = 5;
+
+console.log(food + ' , ' + type + ' , ' + inStock);
 
 
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
 
+inStock = (inStock * 2);
+console.log(inStock);
 
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
 
+inStock = (inStock - 1);
+console.log(inStock);
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 
-
+var isFavorite = false;
+console.log(isFavorite);
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+function shouldOrderMore(inStock) {
+    if (inStock < 20) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+console.log(shouldOrderMore(inStock));
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,9 +60,19 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
 
+var stockCount = 3;
+function chooseToEat(stockCount) {
+    if (stockCount >= 1) {
+    return `I am going to eat some ${food}!`
+  } else {
+    return `We need to get some more ${food}.`
+  }
+}
+
+console.log(chooseToEat(stockCount));
 
 
 // Express the following in code: If the variable "isFavorite" is true,
@@ -50,7 +80,15 @@
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
+function talkAboutFood(isFavorite) {
+  if (isFavorite === true) {
+    return `My favorite food is ${food}!`;
+  } else {
+    return `I recently ate ${food}.`;
+  }
+}
 
+console.log(talkAboutFood(isFavorite));
 
 //------------------------- Arrays -------------------------//
 
@@ -58,17 +96,26 @@
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
 
+var vegetables = ['Spinach', 'Parsnip', 'Zucchini', 'Onion', 'Cucumber'];
+
+console.log(vegetables);
+
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
+
+console.log(vegetables[2]);
 
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
 
+var fruits = ['Cherries', 'Raspberries', 'Bananas', 'Strawberries', 'Blackberries', 'Oranges', 'Grapes'];
+console.log(fruits);
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+console.log(fruits[4]);
 
 //------------------------- Objects -------------------------//
 
@@ -76,7 +123,13 @@
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
 
-
+var groceryStore = {
+  name: 'King Soopers',
+  isOpen: true,
+};
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+groceryStore.location = 'Denver, Colorado';
+console.log(groceryStore);
