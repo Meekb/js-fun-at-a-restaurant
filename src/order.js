149cc6af3210a21a23458c6f91bb23bfbc54f7f0
Beth <>
2021-02-05 14:40:31 -0700

    Add order.js in progress

--- src/order.js
+++ src/order.js
@@ -1,7 +1,24 @@
+function takeOrder(order, deliveryOrders, takeOutOrders) {
+  if (deliveryOrders !== undefined && deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+    return deliveryOrders;
+  } else if (deliveryOrders !== undefined && deliveryOrders.length >= 3) {
+    return deliveryOrders;
+  } else if (deliveryOrders === undefined && takeOutOrders.length < 3) {
+    takeOutOrders.push(order);
+    return takeOutOrders;
+  } else {
+    return takeOutOrders;
+  }
+}
+
+function refundOrder() {
+  
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
