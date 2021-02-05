0fb3243e01b0d9e1041583e34ad907d0e6259044
Beth <>
2021-02-05 08:53:10 -0700

    Add meal.js code passing tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
 
+
+function createMenuItem(menuItemName, price, type) {
+  menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+}
+
+
+function addIngredients(newIngredient, ingredients) {
+      if (ingredients.includes(newIngredient)) {
+        return ingredients
+      } else {
+        return ingredients.push(newIngredient);
+      }
+    }
+
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+
+function decreasePrice(initialPrice) {
+  return initialPrice - (initialPrice * 0.10);
+}
+
+
+function createRecipe(menuItem, ingredients, menuItemType) {
+   recipe = {
+     title: menuItem,
+     ingredients: ingredients,
+     type: menuItemType,
+   };
+   return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
