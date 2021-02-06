a0cddada1ffce9c26916e78d18e0e1a6a0de8941
Beth <>
2021-02-06 16:23:07 -0700

    Add restaurant.js code in progress

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,42 @@
+function createRestaurant(name) {
+  newRestaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
+  return newRestaurant;
+}
+
+function addMenuItem(newRestaurant, menuItem) {
+  if (menuItem.type === 'breakfast' && newRestaurant.menus.breakfast.includes(menuItem) === false) {
+    newRestaurant.menus.breakfast.push(menuItem);
+    return newRestaurant;
+  } else if (menuItem.type === 'lunch' && newRestaurant.menus.lunch.includes(menuItem) === false) {
+    newRestaurant.menus.lunch.push(menuItem);
+    return newRestaurant;
+  } else if (menuItem.type === 'dinner' && newRestaurant.menus.dinner.includes(menuItem) === false) {
+    return newRestaurant.menus.dinner.push(menuItem);
+  } else {
+    return newRestaurant;
+  }
+}
+
+function removeMenuItem(newRestaurant, menuItem) {
+  if (menuItem.type === 'breakfast') {
+    newRestaurant.menus.breakfast.pop(menuItem);
+    return `No one is eating our ${menuItem} - it has been removed from the breakfast menu!`;
+  } else if (menuItem.type === 'dinner') {
+    newRestaurant.menus.dinner.pop(menuItem);
+    return `No one is eating our ${menuItem} - it has been removed from the dinner menu!`;
+  }
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
